7497757c362b6548734a25687bb637ad13208454
Renae Bastholm <renae@dizzybee.com.au>
2012-04-06 22:46:02 +0800

    Added views and page for podcast type.

--- sites/all/themes/aitsl/templates/page.tpl.php
+++ sites/all/themes/aitsl/templates/page.tpl.php
@@ -193,15 +193,21 @@
     <?php endif; ?>
 
     <div id="content" class="column"><div class="section">
+
       <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
+
+      <!-- WE DONT WANT TO DISPLAY THE TITLE FOR PODCAST NODES -->
+      <?php if (isset($node->type)) { if ($node->type != "podcast") { ?>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="title" id="page-title">
+            <?php print $title; ?>
+          </h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+      <?php }} ?>
+
       <?php if ($tabs): ?>
         <div class="tabs">
           <?php print render($tabs); ?>
